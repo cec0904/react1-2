e59571bb68ed8bc2ed7d69c9c80de2bdf4c4e906
cec0904 <cec0904@naver.com>
2024-03-27 16:45:50 +0900

    init

--- test/src/index.js
+++ test/src/index.js
@@ -4,11 +4,14 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Library from "./chapter03/Library";
+import Clock from "./chapter03/Clock";
+import Hello from "./Hello";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <Library />
+    <Hello />
   </React.StrictMode>
 );
 
