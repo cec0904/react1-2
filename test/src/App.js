95d2f074cbe11f3e5fd3341098bd44de064c4720
cec0904 <cec0904@naver.com>
2024-04-24 11:25:09 +0900

    init

--- test/src/App.js
+++ test/src/App.js
@@ -1,13 +1,34 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import Welcome from "./Welcome";
+import Clock from "./chapter03/Clock";
+import Comment from "./chapter03/Comment";
+import CommentList from "./chapter03/CommentList";
+
 function App() {
   return (
-    <div>
-      <h1>당신은 어떤 과일을 좋아하나요?</h1>
+    <div className="App">
+      <CommentList />
+      <Welcome name="홍길동" />
+      <Welcome name="이순신" />
+      <Welcome name="강감찬" />
+      <Clock />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
- <div class="container">
- <img id="img" src="./images/eiffel.jpg" />
- <button>toggle</button>
-</div>
-
   );
 }
 
--- test/src/index.js
+++ test/src/index.js
@@ -1,10 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import Library from "./chapter03/Library";
+import Clock from "./chapter03/Clock";
+import Hello from "./Hello";
+import Welcome from "./Welcome";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
