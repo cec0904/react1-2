17bab3f1b99bec1afe5d6c782c41afea8114b74d
cec0904 <cec0904@naver.com>
2024-04-24 11:43:02 +0900

    init

--- test/midterm/src/App.js
+++ test/midterm/src/App.js
@@ -1,7 +1,17 @@
+import React from "react";
+import image1 from "./apple.jpg";
+import image2 from "./banana.jpg";
+import image3 from "./orange.jpg";
+
 function App() {
   return (
     <div>
-      <h1>Welcome back!</h1>
+      <h1>당신은 어떤 과일을 좋아하나요?</h1>
+      <div>
+        <button className="btn1">사과</button>
+        <button className="btn2">오렌지</button>
+        <button className="btn3">바나나</button>
+      </div>
     </div>
   );
 }
