ea279af0b3a3e99a4787c6df59ce389d0f8ae1b5
cec0904 <cec0904@naver.com>
2024-04-24 11:16:40 +0900

    init

--- test/src/App.js
+++ test/src/App.js
@@ -1,33 +1,7 @@
-import logo from "./logo.svg";
-import "./App.css";
-import React from "react";
-import Welcome from "./Welcome";
-import Clock from "./chapter03/Clock";
-import Comment from "./chapter03/Comment";
-import CommentList from "./chapter03/CommentList";
-
 function App() {
   return (
-    <div className="App">
-      <CommentList />
-      <Welcome name="홍길동" />
-      <Welcome name="이순신" />
-      <Welcome name="강감찬" />
-      <Clock />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome back!</h1>
     </div>
   );
 }
